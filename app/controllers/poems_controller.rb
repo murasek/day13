776863efd8cbd27619884d66d87b565ac7521bbc
koichimurase <k01_mura@yahoo.co.jp>
2017-05-24 12:58:14 +0000

    20170524001

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,10 +1,12 @@
 class PoemsController < ApplicationController
-  before_action :set_poem, only: [:show, :edit, :destroy]
+  
+  before_action :set_poem, only: [:show, :edit,:update,:destroy]
 
   # GET /poems
   # GET /poems.json
   def index
-    @poems = Poem.all
+   @poems = Poem.all
+   
   end
 
   # GET /poems/1
@@ -14,7 +16,9 @@ class PoemsController < ApplicationController
 
   # GET /poems/new
   def new
-    @poem = Poem.new
+    
+      @poem = Poem.new
+    
   end
 
   # GET /poems/1/edit
@@ -24,8 +28,8 @@ class PoemsController < ApplicationController
   # POST /poems
   # POST /poems.json
   def create
-    @poem = Poem.new(poem_params)
-
+    @poem = Poem.new(poems_params)
+      
     respond_to do |format|
       if @poem.save
         format.html { redirect_to @poem, notice: 'Poem was successfully created.' }
@@ -41,7 +45,7 @@ class PoemsController < ApplicationController
   # PATCH/PUT /poems/1.json
   def update
     respond_to do |format|
-      if @poem.update(poem_params)
+      if @poem.update(poems_params)
         format.html { redirect_to @poem, notice: 'Poem was successfully updated.' }
         format.json { render :show, status: :ok, location: @poem }
       else
@@ -53,22 +57,24 @@ class PoemsController < ApplicationController
 
   # DELETE /poems/1
   # DELETE /poems/1.json
-  def destroy
+   def destroy
     @poem.destroy
     respond_to do |format|
-      format.html { redirect_to poems_url, notice: 'Poem was successfully destroyed.' }
+      format.html { redirect_to root_path, notice: 'Poem was successfully destroyed.' }
       format.json { head :no_content }
     end
-  end
+   end
+  
 
   private
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def poems_params
+      params.require(:poem).permit(:title,:content,:auther)
+    end
     # Use callbacks to share common setup or constraints between actions.
     def set_poem
       @poem = Poem.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def poem_params
-      params.require(:poem).permit(:content, :auther)
-    end
+  
 end
